df171a8a608127eca7271c6a56d2275786f5ad56
Crude Oil <crude@croil.net>
2025-06-14 15:32:29 +0200

    add component for db memory

---
+++ Content.Server/Animals/Components/ParrotDbMemoryComponent.cs
@@ -0,0 +1,30 @@
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Server.Animals.Components;
+
+/// <summary>
+/// Makes an entity have a persistent memory of messages for use with a ParrotSpeakerComponent
+/// </summary>
+[RegisterComponent]
+[AutoGenerateComponentPause]
+public sealed partial class ParrotDbMemoryComponent : Component
+{
+    /// <summary>
+    /// How often the persistent memory "refreshes", loading new cross-round messages into a parrot memory
+    /// </summary>
+    [DataField]
+    public TimeSpan RefreshInterval = TimeSpan.FromMinutes(10f);
+
+    /// <summary>
+    /// The next time at which this component will refresh the memory
+    /// </summary>
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
+    [AutoPausedField]
+    public TimeSpan NextRefresh = TimeSpan.Zero;
+
+    /// <summary>
+    /// Minimum playtime requirement for a player before their messages can be committed to persistent memory
+    /// </summary>
+    [DataField]
+    public TimeSpan MinimumSourcePlaytime = TimeSpan.FromHours(5f);
+}

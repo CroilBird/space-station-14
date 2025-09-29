b3d010bf9a9ae12dea4b5adefdc7c64c9b3495a2
Crude Oil <crude@croil.net>
2025-09-29 21:17:15 +0200

    add comment

--- Content.Shared/Silicons/Bots/SecurityThreatComponent.cs
+++ Content.Shared/Silicons/Bots/SecurityThreatComponent.cs
@@ -1,5 +1,9 @@
 namespace Content.Shared.Silicons.Bots;
 
+/// <summary>
+/// Component to indicate some form of a security threat
+/// Used by security bots to determine who to stuncuff
+/// </summary>
 [RegisterComponent, AutoGenerateComponentState]
 public sealed partial class SecurityThreatComponent : Component
 {
